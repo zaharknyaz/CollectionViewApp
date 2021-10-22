7fc1517a6cff2606f910deed2d970aa594aca691
Zahar Knyazev <knyazzz1989@mail.ru>
2021-10-22 22:42:18 +0400

    Initial Commit

---
+++ CollectionViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewApp
+//
+//  Created by Захар Князев on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
